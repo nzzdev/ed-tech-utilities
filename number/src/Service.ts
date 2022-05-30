002f19ce6609833a09c850afbbc9bd4728050598
Michel Grautstueck <michel.grautstueck@nzz.ch>
2022-05-30 16:27:20 +0200

    Add formatNumber and tests

--- number/src/Service.ts
+++ number/src/Service.ts
@@ -1,8 +1,30 @@
-// Note: This is an example file
-function someFunction() {
-  const value = "hello world";
+// Viertelgeviert Leerschlag
+const thinSpace = "\u2009";
+const thousandSeparator = thinSpace;
 
-  return value;
+function formatNumber(
+  number: number,
+  forceThousandSeparator = false,
+  locale = "de-CH"
+): string {
+  // By default we don't use a thousand separator for 4 digit before decimal numbers
+  const intFormatterThreshold = forceThousandSeparator ? 4 : 5;
+  const formatter = new Intl.NumberFormat(locale);
+  let parts = formatter.formatToParts(number);
+  let formattedNumber: string;
+
+  if (Math.floor(number).toString().length >= intFormatterThreshold) {
+    formattedNumber = parts
+      .map((part) => (part.type === "group" ? thousandSeparator : part.value))
+      .join("");
+  } else {
+    // Remove thousand separator if existing
+    parts = parts.filter((part) => part.type !== "group");
+    formattedNumber = parts.map((part) => part.value).join("");
+  }
+
+  return formattedNumber;
 }
 
-export { someFunction };
+// Export of thousandSeparator for unit tests
+export { formatNumber, thousandSeparator };
--- number/test/Service.spec.ts
+++ number/test/Service.spec.ts
@@ -1,6 +1,7 @@
 import * as Lab from "@hapi/lab";
 import { expect } from "@hapi/code";
-import { someFunction } from "../src";
+import { formatNumber } from "../src";
+import { thousandSeparator } from "../src/Service";
 
 // Hapi Lab init code
 const lab = Lab.script();
@@ -10,10 +11,38 @@ export { lab };
 describe("Service", () => {
   before(() => {});
 
-  it("should return 'hello world'", () => {
-    const result = someFunction();
-    const expectedResult = "hello world";
+  it("should format numbers according to the NZZ defaults", () => {
+    // No thousand separator, 'de-CH' localized decimal separator '.'
+    const input = 999.99;
+    const result = formatNumber(input);
+    const expectedResult = "999.99";
+    expect(result).to.equal(expectedResult);
 
+    // No thousand separator, removal of trailing decimal zeroes
+    const input2 = 1000.0;
+    const result2 = formatNumber(input2);
+    const expectedResult2 = "1000";
+    expect(result2).to.equal(expectedResult2);
+
+    // Thousand separator
+    const input3 = 99999;
+    const result3 = formatNumber(input3, true);
+    const expectedResult3 = `99${thousandSeparator}999`;
+    expect(result3).to.equal(expectedResult3);
+
+    // Forced thousand separator on 4 digit integer
+    const input4 = 1000;
+    const result4 = formatNumber(input4, true);
+    const expectedResult4 = `1${thousandSeparator}000`;
+    expect(result4).to.equal(expectedResult4);
+  });
+
+  it("should format a number with 'de-DE' localization", () => {
+    // Forced thousand separator on 4 digits before decimal number,
+    // 'de-DE' localized decimal separator ','
+    const input = 1999.99;
+    const result = formatNumber(input, true, "de-DE");
+    const expectedResult = `1${thousandSeparator}999,99`;
     expect(result).to.equal(expectedResult);
   });
 });
