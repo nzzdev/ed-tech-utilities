d593b83e6e98c15b35ccf3bd6a0eb5694d1fbb3d
Michel Grautstueck <michel.grautstueck@nzz.ch>
2022-06-01 16:27:53 +0200

    Remove clutter from jest config

--- sophie/jest.config.ts
+++ sophie/jest.config.ts
@@ -21,20 +21,4 @@ config.globals = {
   },
 };
 
-//config.transformIgnorePatterns = ["/node_modules/d3-scale"];
-
-//moduleNameMapper: { "^d3-(.*)$": `d3-$1/dist/d3-$1` },
-//moduleNameMapper: { "d3-scale": "d3-scale/dist/d3-scale.js" },
-//transformIgnorePatterns: ["node_modules/(?!(d3-scale)/)"],
-//transformIgnorePatterns: ["/node_modules/d3-scale"],
-
 export default config;
-
-/* "moduleNameMapper": {
-  "d3-scale": "<rootDir>/node_modules/d3-scale/dist/d3-scale.js",
-  "d3-array": "<rootDir>/node_modules/d3-array/dist/d3-array.js",
-  "d3-interpolate": "<rootDir>/node_modules/d3-interpolate/dist/d3-interpolate.js",
-  "d3-color": "<rootDir>/node_modules/d3-color/dist/d3-color.js",
-  "d3-format": "<rootDir>/node_modules/d3-format/dist/d3-format.js",
-  "d3-time": "<rootDir>/node_modules/d3-time/dist/d3-time.js"
-} */
