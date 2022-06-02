51861c23836e740b59b918c1e504c4a2d752483a
Michel Grautstueck <michel.grautstueck@nzz.ch>
2022-06-02 13:27:32 +0200

    Replace hapi/lab &/code with jest in number package

--- number/test/Service.spec.ts
+++ number/test/Service.spec.ts
@@ -1,40 +1,31 @@
-import * as Lab from "@hapi/lab";
-import { expect } from "@hapi/code";
 import { formatNumber } from "../src";
 import { thousandSeparator } from "../src/Service";
 
-// Hapi Lab init code
-const lab = Lab.script();
-const { describe, it, before } = lab;
-export { lab };
-
 describe("Service", () => {
-  before(() => {});
-
   it("should format numbers according to the NZZ defaults", () => {
     // No thousand separator, 'de-CH' localized decimal separator '.'
     const input = 999.99;
     const result = formatNumber(input);
     const expectedResult = "999.99";
-    expect(result).to.equal(expectedResult);
+    expect(result).toEqual(expectedResult);
 
     // No thousand separator, removal of trailing decimal zeroes
     const input2 = 1000.0;
     const result2 = formatNumber(input2);
     const expectedResult2 = "1000";
-    expect(result2).to.equal(expectedResult2);
+    expect(result2).toEqual(expectedResult2);
 
     // Thousand separator
     const input3 = 99999;
     const result3 = formatNumber(input3, true);
     const expectedResult3 = `99${thousandSeparator}999`;
-    expect(result3).to.equal(expectedResult3);
+    expect(result3).toEqual(expectedResult3);
 
     // Forced thousand separator on 4 digit integer
     const input4 = 1000;
     const result4 = formatNumber(input4, true);
     const expectedResult4 = `1${thousandSeparator}000`;
-    expect(result4).to.equal(expectedResult4);
+    expect(result4).toEqual(expectedResult4);
   });
 
   it("should format a number with 'de-DE' localization", () => {
@@ -43,6 +34,6 @@ describe("Service", () => {
     const input = 1999.99;
     const result = formatNumber(input, true, "de-DE");
     const expectedResult = `1${thousandSeparator}999,99`;
-    expect(result).to.equal(expectedResult);
+    expect(result).toEqual(expectedResult);
   });
 });
