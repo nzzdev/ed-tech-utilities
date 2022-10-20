155c604b51ab9777c1e08669bb8da62353951bb3
Alex Kräuchi <al@soup.one>
2022-10-20 13:43:42 +0200

    Create build

---
+++ validation/dist/mjs/isRgbaString.js
@@ -0,0 +1,7 @@
+export const isRgbaString = (rgba) => {
+    if (!rgba || typeof rgba !== "string" || !rgba.length)
+        return false;
+    // source: https://gist.github.com/sethlopezme/d072b945969a3cc2cc11
+    const regex = /^rgba\((0|255|25[0-4]|2[0-4]\d|1\d\d|0?\d?\d),(0|255|25[0-4]|2[0-4]\d|1\d\d|0?\d?\d),(0|255|25[0-4]|2[0-4]\d|1\d\d|0?\d?\d),(0|0?\.\d|1(\.0)?)\)$/;
+    return regex.test(rgba);
+};
