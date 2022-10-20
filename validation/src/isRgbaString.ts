be33f8f596a9446774eaea69069f08f299db8e54
Alex Kräuchi <al@soup.one>
2022-10-20 14:28:49 +0200

    Replace regex

--- validation/src/isRgbaString.ts
+++ validation/src/isRgbaString.ts
@@ -1,9 +1,9 @@
 export const isRgbaString = (rgba: String): boolean => {
   if (!rgba || typeof rgba !== "string" || !rgba.length) return false;
 
-  // source: https://gist.github.com/sethlopezme/d072b945969a3cc2cc11
+  // source: https://rgxdb.com/r/GFYPX74
   const regex: RegExp =
-    /^rgba\((0|255|25[0-4]|2[0-4]\d|1\d\d|0?\d?\d),(0|255|25[0-4]|2[0-4]\d|1\d\d|0?\d?\d),(0|255|25[0-4]|2[0-4]\d|1\d\d|0?\d?\d),(0|0?\.\d|1(\.0)?)\)$/;
+    /rgba\(\s*(-?\d+|-?\d*\.\d+(?=%))(%?)\s*,\s*(-?\d+|-?\d*\.\d+(?=%))(\2)\s*,\s*(-?\d+|-?\d*\.\d+(?=%))(\2)\s*,\s*(-?\d+|-?\d*.\d+)\s*\)/;
 
   return regex.test(rgba);
 };
