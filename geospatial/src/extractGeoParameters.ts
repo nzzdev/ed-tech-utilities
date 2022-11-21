9d214d9e97ae67d11eaca5dcc7f27e74645405c8
Alex Kräuchi <al@soup.one>
2022-11-21 08:30:12 +0100

    Mark for code refactoring

--- geospatial/src/extractGeoParameters.ts
+++ geospatial/src/extractGeoParameters.ts
@@ -3,27 +3,44 @@ import { geoAlbersUsa, geoMercator, geoRobinson } from 'd3-geo-projection';
 import { Feature, FeatureCollection, Geometry } from 'geojson';
 import { feature } from 'topojson-client';
 import { Topology } from 'topojson-specification';
-import { BaseMapVersion, GeoParameters, TopologyObjectProperties } from './interfaces';
+import { GeoParameterConfig, GeoParameters, TopologyObject } from './interfaces';
 
-export const extractGeoParameters = (baseMap: BaseMapVersion, width: number, maxHeight: number): GeoParameters => {
-  if (!baseMap) return undefined;
+export const extractGeoParameters = (
+  baseMap: any,
+  width: number,
+  maxHeight: number,
+  config?: GeoParameterConfig
+): GeoParameters => {
+  if (!baseMap || !baseMap.entities) return undefined;
+  // TODO Throw error: in general rather throw errors early
 
-  const features = getFeatureCollection(baseMap.entities, 'features');
-  const outlines = getFeatureCollection(baseMap.entities, 'outlines');
-  const water = getFeatureCollection(baseMap.entities, 'water');
-  let projection = getProjection(baseMap).fitWidth(width, features);
+  // TODO no default topology objects
+  let topologyObjectNames = ['features', 'outlines', 'water'];
+  // Add additional topology objects specified in config
+  if (config?.topologyObjectNames?.length)
+    topologyObjectNames = topologyObjectNames.concat(
+      config.topologyObjectNames.filter((obj) => !topologyObjectNames.includes(obj))
+    );
+
+  // create topologyObject for each topologyObjectName
+  const topologyObjects: TopologyObject = {};
+  topologyObjectNames.forEach((name) => (topologyObjects[name] = getFeatureCollection(baseMap.entities, name)));
+
+  // REVIEW we are currently using `features` to determine a whole bunch of stuff below. Should it be possible to use something else? See Solardächer Project --> yes
+  let projection = getProjection(baseMap).fitWidth(width, topologyObjects.features);
   let path = geoPath(projection);
-  let bounds = path.bounds(features);
+  let bounds = path.bounds(topologyObjects.features);
   const height = bounds[1][1];
 
   if (height > maxHeight) {
-    projection = getProjection(baseMap).fitHeight(maxHeight, features);
+    projection = getProjection(baseMap).fitHeight(maxHeight, topologyObjects.features);
     path = geoPath(projection);
-    bounds = path.bounds(features);
+    bounds = path.bounds(topologyObjects.features);
   }
 
-  if (features.type === 'FeatureCollection') {
-    for (const feature of features.features) {
+  // TODO move all this below to it's own exported function
+  if (topologyObjects.features?.type === 'FeatureCollection') {
+    for (const feature of topologyObjects.features.features) {
       if (feature.properties.centroid_lat && feature.properties.centroid_lon) {
         // If we already have a centroid, use that
         feature.properties.centroidPlanar = projection([
@@ -40,7 +57,7 @@ export const extractGeoParameters = (baseMap: BaseMapVersion, width: number, max
     }
   }
 
-  return { path, bounds, features, outlines, water } as GeoParameters;
+  return { path, bounds, topologyObjects } as GeoParameters;
 };
 
 const getProjection = (baseMap): GeoProjection => {
@@ -50,10 +67,11 @@ const getProjection = (baseMap): GeoProjection => {
     return geoAlbersUsa();
   } else {
     return geoMercator();
+    // TODO throw if none of these
   }
 };
 
-const getFeatureCollection = (topology: Topology, objectName: TopologyObjectProperties) => {
+const getFeatureCollection = (topology: Topology, objectName: string) => {
   if (topology && topology.objects[objectName]) {
     return feature(topology, topology.objects[objectName]) as FeatureCollection<Geometry, { [name: string]: any }>;
   }
--- geospatial/src/interfaces.ts
+++ geospatial/src/interfaces.ts
@@ -1,51 +1,54 @@
 import { GeoPath } from 'd3-geo';
 import { FeatureCollection } from 'geojson';
 
+// TODO Rewrite baseMap according to JSONs stored in DB
 // REVIEW Should we export types from this package? E.g. Basemap
-export interface BaseMap {
-  id: string;
-  title: string;
-  versions: BaseMapVersion[];
-}
+// export interface BaseMap {
+//   id: string;
+//   title: string;
+//   versions: BaseMapVersion[];
+// }
 
-export interface BaseMapVersion {
-  validFrom: string;
-  data: {
-    source?: {
-      url: string;
-      label: string;
-    };
-    config: BaseMapConfig;
-  };
-  entities: any; // TODO type
-}
+// export interface BaseMapVersion {
+//   validFrom: string;
+//   source?: {
+//     url: string;
+//     label: string;
+//   };
+//   config: BaseMapConfig;
+//   entities: any; // TODO
+// }
 
-export interface BaseMapConfig {
-  defaultEntityType: string;
-  displayEntityType?: string;
-  entityTypes: EntityTypes;
-  projection: string;
-}
+// export interface BaseMapConfig {
+//   defaultEntityType: string;
+//   displayEntityType?: string;
+//   entityTypes: EntityTypes;
+//   projection: string;
+// }
 
-export interface EntityTypes {
-  name?: string;
-  id?: string;
-  nuts?: string;
-  bfsNumber?: string;
-  code?: string;
-  key?: string;
-  ags?: string;
-  DEP?: string;
-  iso3?: string;
-  isoCode?: string;
-}
+// export interface EntityTypes {
+//   name?: string;
+//   id?: string;
+//   nuts?: string;
+//   bfsNumber?: string;
+//   code?: string;
+//   key?: string;
+//   ags?: string;
+//   DEP?: string;
+//   iso3?: string;
+//   isoCode?: string;
+// }
 
 export interface GeoParameters {
   path: GeoPath;
   bounds: [[number, number], [number, number]];
-  features: FeatureCollection;
-  outlines: FeatureCollection;
-  water: FeatureCollection;
+  topologyObjects: TopologyObject;
 }
 
-export type TopologyObjectProperties = 'features' | 'outlines' | 'water';
+export interface TopologyObject {
+  [name: string]: FeatureCollection;
+}
+
+export interface GeoParameterConfig {
+  topologyObjectNames?: Array<string>;
+}
