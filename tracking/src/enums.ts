eade9270567a0c017931ec1e1b66ece02e32f80e
Alex Kräuchi <al@soup.one>
2023-05-02 14:43:37 +0200

    Add tracking category

--- tracking/src/enums.ts
+++ tracking/src/enums.ts
@@ -1,4 +1,5 @@
 // All events should prefixed with "et-" so they can be found easily in the tracking data and not mixed with older events.
+// Create a new category like "navigation" if your new actions do not fit into the existing categories
 export enum EventActionNavigation {
   SWIPE_RIGHT = "et-swipe-right",
   SWIPE_LEFT = "et-swipe-left",
@@ -6,4 +7,7 @@ export enum EventActionNavigation {
   CLICK_PREVIOUS = "et-click-previous",
 }
 
-// TODO create a new category like "navigation" if your new actions do not fit into the existing categories
+export enum MediaAction {
+  PLAY_AUDIO_THROUGH_HOVER = "et-play-audio-through-hover",
+  PLAY_AUDIO_THROUGH_TOUCH = "et-play-audio-through-touch",
+}
